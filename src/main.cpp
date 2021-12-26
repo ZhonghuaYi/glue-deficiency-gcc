62c6308492c918416a5024ede9f673415e12c6a7
ZhonghuaYi <yizhonghua6@gmail.com>
2021-12-26 17:16:45 +0800

    feat: 5:16PM, 12/26/2021.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 
+using namespace std;
+using namespace cv;
+
+vector<string> REFER_LIST = { "test000.bmp" };
+vector<string> DEFECT_LIST = { "test001.bmp", "test002.bmp" };
+int PRE_AREA_NUM = 12;
+
 int main() {
-    using namespace std;
-    using namespace cv;
-    string img_path = "../image/test000.bmp";
-    Mat image;
-    image = imread(img_path, 1);
-    namedWindow("img", WINDOW_AUTOSIZE);
-    imshow("img", image);
-    std::cout << "Hello, World!" << std::endl;
+    vector<string> sample;
+    sample.insert(sample.end(), REFER_LIST.begin(), REFER_LIST.end());
+    sample.insert(sample.end(), DEFECT_LIST.begin(), DEFECT_LIST.end());
+
+    int count = 1;
+    Mat structure_element = getStructuringElement(MORPH_RECT, Size(7, 7));
+    vector<int> target_region_areas;
+
+    vector<string>::iterator i;
+    for (i = sample.begin(); i != sample.end(); ++i) {
+        Mat image = imread(*i, 1);
+        Mat hist = calcHist()
+    }
+
+    for (vector<int>::size_type i = 0; i < target_region_areas.size(); ++i)
+        cout << target_region_areas[i] << endl;
     waitKey(0);
     return 0;
 }
