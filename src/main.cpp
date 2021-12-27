1d203b871ec5afe91e17b24cf6f8110ab7db68ec
ZhonghuaYi <yizhonghua6@gmail.com>
2021-12-27 15:56:12 +0800

    feat: 3:56PM, 12/27/2021.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <opencv2/opencv.hpp>
 
@@ -9,18 +9,45 @@ vector<string> REFER_LIST = { "test000.bmp" };
 vector<string> DEFECT_LIST = { "test001.bmp", "test002.bmp" };
 int PRE_AREA_NUM = 12;
 
+/// <summary>
+/// get normalized histogram.
+/// </summary>
+/// <param name="img"></param>
+/// <returns>
+/// normalized histogram of input image.
+/// </returns>
 Mat getHistogram(const Mat& img) {
     Mat hist = Mat::zeros(Size(256, 1), CV_32FC1);
-    Mat img_size, out;
-    multiply(Mat::ones(hist.size(), hist.depth()), img.total(), img_size, 1, CV_32FC1);
+    Mat out;
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
             int index = int(img.at<uchar>(i, j));
             hist.at<float>(0, index) += 1;
         }
     }
-    divide(hist, img_size, out);
+    out = hist / img.total();
     return out;
+}
+
+/// <summary>
+/// get the cumulative distribution fuction(CDF) of image.
+/// </summary>
+/// <param name="in_pic_histogram"></param>
+/// <returns>
+/// CDF of the image.
+/// </returns>
+Mat cdf(const Mat& in_pic_histogram) {
+    int hist_size = in_pic_histogram.total();
+    Mat transform = Mat::zeros(in_pic_histogram.size(), in_pic_histogram.depth());
+    float temp = 0.;
+    for (int i = 0; i < hist_size; ++i) {
+        temp += in_pic_histogram.at<float>(0, i);
+        transform.at<float>(0, i) = temp;
+    }
+    transform = transform * (hist_size - 1);
+    transform.convertTo(transform, CV_32SC1);
+    transform.convertTo(transform, in_pic_histogram.depth());
+    return transform;
 }
 
 int main() {
@@ -34,18 +61,37 @@ int main() {
     vector<int> target_region_areas;
 
     vector<string>::iterator i;
-    for (i = sample.begin(); i != sample.end(); ++i) {
-        Mat image = imread(img_root_path + *i, 1);
+    for (string i :sample) {
+        Mat image = imread(img_root_path + i, 0);
         Mat hist;
-        vector<float> range = { 0, 256 };
-        //calcHist(&image, 1, 0, Mat(), hist, 1, 256, range);
+        
+        /*
+        * opencv计算直方图的官方方法
+        int channels[] = { 0 };
+        const int histSize[] = { 256 };
+        float range[] = { 0, 256 };
+        const float* ranges[] = { range };
+        calcHist(&image, 1, channels, Mat(), hist, 1, histSize, ranges);
+        */
+
+        /*
+        
+        */
         hist = getHistogram(image);
-            
+        //cout << hist << endl;
+        Mat img_cdf = cdf(hist) / 255.;
+        int point[2] = {};
+        minMaxIdx(abs(img_cdf - 0.3), 0, 0, point, 0);
+        int index = point[1];
+
+        namedWindow("img");
+        imshow("img", image);
         break;
     }
 
     for (int target_region_area : target_region_areas)
         cout << target_region_area << endl;
     waitKey(0);
+    destroyAllWindows();
     return 0;
 }
