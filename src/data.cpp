27eace18b0aebfcf0c0b56bacf1558aa1bedc9fa
ZhonghuaYi <yizhonghua6@gmail.com>
2022-04-13 20:54:43 +0800

    feat(*): 8:54PM, 4/13/2022.

--- src/data.cpp
+++ src/data.cpp
@@ -11,9 +11,6 @@ void data::LoadData(int sample_set) {
     using namespace cv;
     using namespace func;
     if (sample_set == 1){
-        vector<Mat> edge_templates;
-        vector<Mat> templates;
-
         string segment = "template_match";
         // 样本图片的位置
         string sample_root = "./image/sample/";
@@ -21,22 +18,42 @@ void data::LoadData(int sample_set) {
         string refer2_root = "./image/refer2/";
 
         // 读取样本
-        vector<string> sample = func::sampleGenerate(sample_root);
+        extern vector<string> sample;
+        sample = func::sampleGenerate(sample_root);
 
         // 读取参考样本
         vector<string> refer1_sample = func::referGenerate(refer1_root);
         vector<string> refer2_sample = func::referGenerate(refer2_root);
 
         if (segment == "thresh_segment"){
-            float area_percent = 0.3;
-            int normal_area = 420;
-            float thresh = 0.9;
+            extern float area_percent, thresh;
+            extern int normal_area;
+            area_percent = 0.3;
+            normal_area = 420;
+            thresh = 0.9;
 
             // 用于形态学计算的矩形结构元素
-            Mat structure_element = cv::getStructuringElement(MORPH_RECT, Size(7, 7));
+            extern Mat structure_element;
+            structure_element = cv::getStructuringElement(MORPH_RECT, Size(7, 7));
         }
         else if(segment == "template_match"){
-            int canny[2] = {50, 100};
+            extern vector<Mat> edge_templates, templates;
+            int canny1[2] = {50, 100};
+            int canny2[2] = {10, 200};
+            extern vector<int*> canny;
+            canny.push_back(canny1);
+            canny.push_back(canny2);
+            extern string f;
+            f = "sift";
+
+            // 生成模板
+            vector<Mat> t_out;
+            t_out = func::templateGenerate(refer1_sample, Range(50, 300), Range(50, 300), "canny", canny1);
+            templates.push_back(t_out[0]);
+            edge_templates.push_back(t_out[1]);
+            t_out = func::templateGenerate(refer2_sample, Range(20, 100), Range(220, 470), "canny", canny2);
+            templates.push_back(t_out[0]);
+            edge_templates.push_back(t_out[1]);
         }
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 #include <opencv2/highgui.hpp>
 
 #include "func.h"
+#include "data.h"
+#include "feature.h"
+#include "roi.h"
+
+void thresh_segment(cv::Mat image, float area_percent, cv::Mat structure_element, int normal_area, float thresh){
+
+}
 
 int main() {
 
